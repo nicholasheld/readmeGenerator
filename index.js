f0421424a9e098551326a4c509a1e8f49fd30229
nicholas <nicholaspheld@gmail.com>
2021-04-12 16:58:12 -0500

    removing node fils and added to index js

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 // TODO: Include packages needed for this application
-//const genrateMD = require('./generateMarkdown.js')
+const genrateMarkdown = require('./generateMarkdown.js')
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
